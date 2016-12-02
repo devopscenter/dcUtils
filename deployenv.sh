442637c6bb30c8b82b95f2a807e7e4a35c079f0f
Gregg Jensen <gjensen@devops.center>
2016-12-02 14:16:33 -0600

    move the template files into a templates directory to make them less obvious and modify the deployenv.sh to reflect that change

--- deployenv.sh
+++ deployenv.sh
@@ -162,7 +162,7 @@ BASE_CUSTOMER_APP_UTILS_DIR="${BASE_CUSTOMER_DIR}/${CUSTOMER_APP_NAME}/${CUSTOME
 # first off copy the health_checks template to the appropriate place for the
 # application
 #-------------------------------------------------------------------------------
-cp template-health_checks ${BASE_CUSTOMER_APP_UTILS_DIR}/config/health_checks
+cp templates/health_checks ${BASE_CUSTOMER_APP_UTILS_DIR}/config/health_checks
 
 #-------------------------------------------------------------------------------
 # handle the case where the type is an instance
@@ -278,10 +278,10 @@ else
     # NOTE2 using a ? as the delimiter for the sed command to allow the variables to have
     # a forward slash (ie a directory path) in it.  By using a different delimeter than a
     # slash sed doesn't get confused by the forwards slashes in the variable.
-    sed -e "s?APP_NAME-ENV.env?${TARGET_ENV_FILE}?" template-common.yml > "${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/common.yml"
-    sed -e "s?APP_NAME-ENV.env?${TARGET_ENV_FILE}?" template-docker-compose.yml >  "${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/docker-compose.yml"
+    sed -e "s?APP_NAME-ENV.env?${TARGET_ENV_FILE}?" templates/common.yml > "${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/common.yml"
+    sed -e "s?APP_NAME-ENV.env?${TARGET_ENV_FILE}?" templates/docker-compose.yml >  "${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/docker-compose.yml"
     sed -i -e "s?APP_NAME-CONFIG-DIR-common.yml?\"${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/common.yml\"?" "${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/docker-compose.yml"
-    sed -e "s?APP_NAME-ENV.env?${TARGET_ENV_FILE}?" template-docker-compose-debug.yml >  "${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/docker-compose-debug.yml"
+    sed -e "s?APP_NAME-ENV.env?${TARGET_ENV_FILE}?" templates/docker-compose-debug.yml >  "${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/docker-compose-debug.yml"
     sed -i -e "s?APP_NAME-CONFIG-DIR-common.yml?\"${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/common.yml\"?" "${BASE_CUSTOMER_APP_UTILS_DIR}/config/${ENV}/docker-compose-debug.yml"
 
     #echo "ENV vars for '${ENV}' added to docker-current.env for use by docker compose"
