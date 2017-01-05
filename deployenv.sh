ee35a7e22f4da00fb69e3e7ba099a419e19050b3
Gregg Jensen <gjensen@devops.center>
2017-01-05 13:25:46 -0600

    turn on debugging for this script for this next test run.

--- deployenv.sh
+++ deployenv.sh
@@ -41,7 +41,7 @@
 
 #set -o nounset     # Treat unset variables as an error
 #set -o errexit      # exit immediately if command exits with a non-zero status
-#set -x             # essentially debug mode
+set -x             # essentially debug mode
 
 
 #---  FUNCTION  ----------------------------------------------------------------
