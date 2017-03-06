88b6b31b6ceda59e612b3b6a8629e792a6097611
Gregg Jensen <gregg.jensen@amkanalytics.com>
2017-03-06 17:14:59 -0600

    fix typo on environment

--- deployenv.sh
+++ deployenv.sh
@@ -166,12 +166,12 @@ if [[ $TYPE = "instance" ]]; then
     # duplicate key/value pairs.  See if this would benefit from the same kind of
     # processing that the docker (ie, the else of the TYPE if check) section has.
 
-    # backup the /etc/enviornment so that we don't keep duplicating items past the very first time
+    # backup the /etc/environment so that we don't keep duplicating items past the very first time
     # that we run deployenv.sh on an instance.  That is, the very first time we run this on a 
     # newly created instance it will take whatever is in file and save it, so that can be the
     # first thing put in after we remove it.
     if [[ ! -f /etc/environment.ORIG ]]; then
-        sudo cp /etc/enviornment /etc/environment.ORIG
+        sudo cp /etc/environment /etc/environment.ORIG
     fi
 
     # remove the /etc/environment file so we start from a known point each time
