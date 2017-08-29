fd945ddecfcef2ef66f6285b40b4df971583e25c
Gregg Jensen <gjensen@devops.center>
2017-08-29 10:25:01 -0500

    refactor to capture separate user defined networks per docker-compose.yml

--- start-dc-containers.sh
+++ start-dc-containers.sh
@@ -23,6 +23,8 @@
 #set -o errexit      # exit immediately if command exits with a non-zero status
 #set -x              # essentially debug mode
 
+DEFAULT_SUBNET="172.36.4.0/24"
+
 #---  FUNCTION  ----------------------------------------------------------------
 #          NAME:  usage
 #   DESCRIPTION:  function that gets called to show the user how this script should
@@ -45,6 +47,98 @@ function usage
     echo -e "--debug will start the web-debug container rather than the web one"
     echo
     exit 1
+}
+
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  setupNetwork
+#   DESCRIPTION:  ensures the user defined network for this container is set up
+#    PARAMETERS:
+#       RETURNS:
+#-------------------------------------------------------------------------------
+setupNetwork()
+{
+
+    # get the subnet definition from the customers utils/config/local directory
+    DOCKER_SUBNET_FILE="${BASE_CUSTOMER_DIR}/${dcDEFAULT_APP_NAME}/${CUSTOMER_APP_UTILS}/config/${CUSTOMER_APP_ENV}/docker-subnet.conf"
+
+
+    if [[ -f ${DOCKER_SUBNET_FILE} ]]; then
+        # need to get the docker-subnet.conf from the app-utils/config/local
+        aLine=$(grep DOCKER_SUBNET_TO_USE ${DOCKER_SUBNET_FILE})
+        export DOCKER_SUBNET_TO_USE=${aLine/*=}
+        SUBNET_TO_USE=${DOCKER_SUBNET_TO_USE%\.*}
+    else
+        # choose a default subnet 
+        SUBNET_TO_USE=${DEFAULT_SUBNET}
+    fi
+
+    SERVICES=($(docker-compose -f ${DOCKER_COMPOSE_FILE} config --services 2>&1 > /dev/null))
+    NUM_SERVICES=${#SERVICES[@]}
+    count=0
+    
+    for number in {2..9}
+    do
+        if [[ ${count} -ge ${NUM_SERVICES} ]]; then
+            break
+        fi
+
+        # set up the statis ip
+        export DOCKER_STATIC_IP_$((${number}-1))="${SUBNET_TO_USE}.${number}"
+        count=$((${count}+1))
+
+        # if the os is OSX then we have to set up an alias on lo0 (the interface
+        # that docker talks on) to set up a connection to the container
+        # in linux the bridge is created with an interface that the host can access
+        if [[ ${OSNAME} == "Darwin" ]]; then
+            interfaceOutput=$(ifconfig lo0 | grep "${SUBNET_TO_USE}.${number}")
+            if [[ -z ${interfaceOutput} ]]; then
+                sudo ifconfig lo0 alias "${SUBNET_TO_USE}.${number}"
+            fi
+        fi
+    done
+}
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  findContainerName
+#   DESCRIPTION:  loops through the compose config and searchs for a container
+#                 name that is associated with service name
+#    PARAMETERS:
+#       RETURNS:  container_name value
+#-------------------------------------------------------------------------------
+findContainerName()
+{
+    local serviceName=$1
+
+    ALL_CONFIG=($(docker-compose -f ${DOCKER_COMPOSE_FILE} config))
+    NUM_LINES=${#ALL_CONFIG[@]}
+    c=0
+    while [[ ${c} -lt ${NUM_LINES} ]]
+    do
+        if [[ ${ALL_CONFIG[$c]} == "services:" ]]; then
+            # now contiue until the current service is found
+            while [[ ${c} -lt ${NUM_LINES} ]]
+            do
+                c=$(($c + 1))
+                if [[ ${ALL_CONFIG[$c]} == "${serviceName}:" ]]; then
+                    while [[ ${c} -lt ${NUM_LINES} ]]
+                    do
+                        c=$(($c + 1))
+                        if [[ ${ALL_CONFIG[$c]} == "container_name:" ]]; then
+                            # its the next one
+                            c=$(($c + 1))
+                            echo ${ALL_CONFIG[${c}]}
+                            return
+                        fi
+                    done
+                fi
+            done
+        fi
+        c=$(($c + 1))
+    done
+    set +x
 }
 
 
@@ -121,39 +215,37 @@ if [[ ! -f ${DOCKER_COMPOSE_FILE} ]]; then
     exit 1
 fi
 
-NUM_NETWORKS=$(docker network ls | grep -sc "_dcnet")
-export NET_NUMBER=$((20+$NUM_NETWORKS))
-#echo "Network subnet used: 172.${NET_NUMBER}.0"
-
+OSNAME=$(uname -s)
+setupNetwork
 
 CMDTORUN="docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${dcDEFAULT_APP_NAME} up -d"
 dcLog  ${CMDTORUN}
 ${CMDTORUN}
 
+
+SERVICES=($(docker-compose -f ${DOCKER_COMPOSE_FILE} config --services))
+for service in ${SERVICES[@]}
+do
+    CONTAINER_NAME=$(findContainerName $service)
+        set -x
+    serviceIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME})
+    hostEntry=$(grep ${CONTAINER_NAME} /etc/hosts)
+
+    if [[ ${hostEntry} ]]; then
+        # it was there so we need to see if the IP is different
+        tmpArray=(${hostEntry})
+        if [[ ${tmpArray[0]} != ${serviceIP} ]]; then
+            # the entry was there but the IP is different
+            #sudo sed -i.bak "/$CONTAINER_NAME/ s/.*/${tmpIP}    $CONTAINER_NAME/" /etc/hosts
+            sudo sed -i.bak "/$CONTAINER_NAME/ s/.*/${serviceIP}    $CONTAINER_NAME/" /etc/hosts
+            #sudo sed -i.bak "s/.*${CONTAINER_NAME}/${tmpIP}    $CONTAINER_NAME/" /etc/hosts
+        fi
+    else
+        # it wasn't there so append it
+        echo "${serviceIP}    ${CONTAINER_NAME} " | sudo tee -a /etc/hosts > /dev/null
+    fi
+        set +x
+done
+
 dcEndLog "Finished..."
 exit
-
-#-------------------------------------------------------------------------------
-#  TODO: figure out a cross platform way to open terminals for the containers
-#        In the meantime have the user use the script ${dcUtils}/enter-container.py
-#        in different terminals.   Or if they want to see the debug logs for any
-#        of the containers use the show-docker-logs.py and select a running container
-#-------------------------------------------------------------------------------
-new_tab() {
-  sleep 1
-  TAB_NAME=$1
-  COMMAND=$2
-  COMMANDINSIDE=$3
-  osascript \
-    -e "tell application \"Terminal\" to activate" > /dev/null
-  sleep 1
-  osascript \
-    -e "tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down" \
-    -e "tell application \"Terminal\" to do script \"$COMMAND\" in selected tab of the front window" > /dev/null
-  sleep 2
-  osascript \
-    -e "tell application \"Terminal\" to do script \"printf '\\\e]1;$TAB_NAME\\\a'; $COMMANDINSIDE\" in selected tab of the front window" > /dev/null
-}
-
-new_tab "Web Container" "cd $dcUTILS && docker exec -it web-1 /bin/bash" " "
-new_tab "Worker Container" "cd $dcUTILS && docker exec -it worker-1 /bin/bash" " "
