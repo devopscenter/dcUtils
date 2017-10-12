57cca4cc9824c41686295967fdbd997c747a28cc
Gregg Jensen <gjensen@devops.center>
2017-10-12 15:12:20 -0500

    remove setting the COLUMNS and LINES before entering the container

--- enter-container.py
+++ enter-container.py
@@ -59,8 +59,7 @@ def main(argv):
     print "\nConnecting to: " + containerToUse
     print
     dockerCmdToRun = "docker exec -it " + containerToUse +  \
-        "  /bin/bash -c 'export TERM=xterm \
-        COLUMNS=$(tput cols) LINES=$(tput lines); exec bash'"
+        "  /bin/bash -c 'export TERM=xterm ; exec bash'"
     subprocess.call(dockerCmdToRun, shell=True)
 
 
